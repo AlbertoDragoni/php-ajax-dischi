5b1b99ffcab2d1d0afbcfa5da650d7636133a36f
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-20 17:26:48 +0200

    lavorazione (1)

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php echo 'ciao'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Comic+Neue&display=swap" rel="stylesheet">
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.3/handlebars.min.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <title>Boolfy</title>
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+        <img src="img/logo.png" alt="logo-spotify">
+      </div>
+    </header>
+      <div class="container-fullwidth">
+        <main>
+          <div class="card">
+          
+          </div>
+        </main>
+      </div>
+  <script src="dist/js/main.js" charset="utf-8"></script>
+  </body>
+</html>
